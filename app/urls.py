a109278e2369de995f9e6e5e9345347ce6c3b816
jeovanagvf <jeovanagvf@gmail.com>
2026-03-19 11:10:26 -0300

    feat: criação da entidade para Livro

--- app/urls.py
+++ app/urls.py
@@ -7,9 +7,10 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, UserViewSet
+from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet, UserViewSet
 
 router = DefaultRouter()
+router.register(r'livro', LivroViewSet, basename='livro')
 router.register(r'categoria', CategoriaViewSet, basename='categoria')
 router.register(r'editora', EditoraViewSet, basename='editora')
 router.register(r'usuarios', UserViewSet, basename='usuarios')
